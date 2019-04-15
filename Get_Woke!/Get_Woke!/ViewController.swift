391a839fc0ca71915b596bcf2c2d43c692296555
Tomoki Takasawa <>
2019-04-15 18:56:55 -0400

    initialization

---
+++ Get_Woke!/Get_Woke!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Get_Woke!
+//
+//  Created by Tomoki Takasawa on 4/15/19.
+//  Copyright © 2019 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
